b82f8c5bd909d452f291ef333a405668f21adff9
Minji Lee <eveygood9521@gmail.com>
2018-01-30 07:07:16 -0500

    Add files via upload

---
+++ fizzbuzz/src/fizzbuzz/main.java
@@ -0,0 +1,30 @@
+package fizzbuzz;
+
+public class main {
+	public static void main(String[] args){
+
+	
+	for(int i=1; i<=100 ; i++){
+		if(i%3==0){
+			if(i%5==0 && i%3==0){
+				System.out.print("FizzBuzz ");
+			}
+			else{
+				System.out.print("Fizz ");
+			}
+		}
+		else if(i%5==0){
+			if(i%5==0 && i%3==0){
+				System.out.print("FizzBuzz ");
+			}
+			else{
+				System.out.print("Buzz ");
+			}
+			
+		}
+		else {
+			System.out.print(i + " ");
+		}
+	}
+}
+}
